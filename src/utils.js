754d24937ef4ff49fdc0b582c4cc1d389eaf4d27
panasthetik2 <panasthetik@gmail.com>
2022-03-28 14:11:02 -0400

    updated contract functions

--- src/utils.js
+++ src/utils.js
@@ -18,9 +18,9 @@ export async function initContract() {
   // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
     // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ["exhibition_count"],
+    viewMethods: ["event_count"],
     // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['add_exhibition', 'add_vote', 'list_exhibitions', 'artist_count', 'add_artist'],
+    changeMethods: ['add_event', 'add_vote', "list_events"],
   })
 }
 
